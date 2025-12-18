c7b1d0156d4a914b70d43f3a87b2b0d69623e298
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-12-18 09:12:06 +0700

    2

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,25 @@
-import { Fragment } from "react";
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  let selectedIndex = 0;
+  useState 
 
- 
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={ selectedIndex === index ? "list-group-item active" : "list-group-item" }
+            key={item}
+            onClick={() => {selectedIndex = index}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
