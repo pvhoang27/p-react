95b3dc00af90cf545fb7c30b89c27cd9f16d2d2d
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-12-18 08:36:14 +0700

    1

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,18 +7,12 @@ function ListGroup() {
     'London',
     'Paris'
   ];
-  items.map(item => <li>{item}</li>)
+  
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item" >
-          An active item
-        </li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map(item => <li>{item}</li>)}
       </ul>
     </>
   );
